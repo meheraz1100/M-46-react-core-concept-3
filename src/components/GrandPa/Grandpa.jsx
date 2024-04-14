d3824f8e812d94c85232dc29a858d35b48eb1448
meheraz1100 <meheraz1100@gmail.com>
2024-04-14 08:36:51 +0600

    create context

--- src/components/GrandPa/Grandpa.jsx
+++ src/components/GrandPa/Grandpa.jsx
@@ -1,22 +1,38 @@
+import { createContext, useState } from "react";
 import Aunty from "../Aunty/Aunty";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
-import "./Grandpa.css"
+import "./Grandpa.css";
+
+export const AssetContaxt = createContext("gold");
+export const MoneyContext = createContext(1000);
 
 const Grandpa = () => {
-    
-    const asset = 'diamond';
+    const [money, setMoney] = useState(1000)
+  const asset = "diamond";
 
-    return (
-        <div className="grandpa">
-            <h2 className="text-2xl">GrandPa</h2>
-            <section className="flex">
-                <Dad asset={asset}></Dad>
-                <Uncle></Uncle>
-                <Aunty></Aunty>
-            </section>
-        </div>
-    );
+  return (
+    <div className="grandpa">
+        <p>Net Money: {money}</p>
+      <h2 className="text-2xl">GrandPa</h2>
+      <MoneyContext.Provider value={[money, setMoney]}>
+        <AssetContaxt.Provider value="gold">
+          <section className="flex">
+            <Dad asset={asset}></Dad>
+            <Uncle asset={asset}></Uncle>
+            <Aunty></Aunty>
+          </section>
+        </AssetContaxt.Provider>
+      </MoneyContext.Provider>
+    </div>
+  );
 };
 
 export default Grandpa;
+
+/**
+ * 1. create a new context and export it
+ * 2. add provider for the contaxt with a value. value be anything
+ * 3. useContext to access value in the context API
+ * 3.
+ */
