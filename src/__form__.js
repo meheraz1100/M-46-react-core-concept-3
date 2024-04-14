2774b67f203e53cbf1c8107282cff5d14e7a1dc8
meheraz1100 <meheraz1100@gmail.com>
2024-04-14 06:53:46 +0600

    some text

--- src/__form__.js
+++ src/__form__.js
@@ -8,4 +8,7 @@
  * 
  * 4. Uncontrolled Element: useref to create a reference to the element and access value by using like : nameRef.current.value
  * 
+ * 5. use hook to handle states and submit and error. example react hook form
+ * 
+ * 5. Reusable usable: 
  */
