b085d690ead873a7d946e66cbf70741bd6b34a18
meheraz1100 <meheraz1100@gmail.com>
2024-04-14 07:39:41 +0600

    component added

---
+++ src/components/Special/Special.jsx
@@ -0,0 +1,10 @@
+const Special = ({asset}) => {
+    return (
+        <div>
+            <h2 className="text-2xl">Special</h2>
+            <p>Has: {asset}</p>
+        </div>
+    );
+};
+
+export default Special;
