d72e4df8f7d2a7b5edb4cb6dfcf5b4d8832a4433
meheraz1100 <meheraz1100@gmail.com>
2024-04-14 06:52:51 +0600

    added reusable form

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,47 @@
-import './App.css'
-import HookForm from './components/HookForm/HookForm'
-import RefForm from './components/RefForm/RefForm'
+import "./App.css";
+// import HookForm from './components/HookForm/HookForm'
+// import RefForm from './components/RefForm/RefForm'
+import ReusableForm from "./components/ReusableForm/ReusableForm";
 // import Simpleform from './components/SimpleForm/Simpleform'
 // import StateFulForm from './components/StateFulForm/StateFulForm'
 
 function App() {
+  const handleSignUpSubmit = (data) => {
+    console.log('Sign up data', data);
+  };
+
+  const handleUpdateProfile = (data) => {
+    console.log('Update profile data', data);
+  };
 
   return (
     <>
       <h1>Vite + React</h1>
       {/* <StateFulForm></StateFulForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      <ReusableForm
+        formTitle={"Sign Up"}
+        submitBtnText="Submit"
+        handleSubmit={handleSignUpSubmit}
+      >
+        <div>
+          <h2 className="text-2xl">Sign Up</h2>
+          <p>Please sign up right now</p>
+        </div>
+      </ReusableForm>
+      <ReusableForm
+        formTitle={"Profile Update"}
+        submitBtnText="Update"
+        handleSubmit={handleUpdateProfile}
+      >
+        <div>
+          <h2 className="text-2xl">Update Profile</h2>
+          <p>Always keep your profile updated</p>
+        </div>
+      </ReusableForm>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
