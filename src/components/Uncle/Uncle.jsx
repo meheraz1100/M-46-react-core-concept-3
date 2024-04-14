0a4ebbbbd9d86ce86c9746577f226e259d2a7375
meheraz1100 <meheraz1100@gmail.com>
2024-04-14 08:42:18 +0600

    get the asset using prop drlling

--- src/components/Uncle/Uncle.jsx
+++ src/components/Uncle/Uncle.jsx
@@ -1,11 +1,11 @@
 import Cousin from "../Cousin/Cousin";
 
-const Uncle = () => {
+const Uncle = ({asset}) => {
     return (
         <div>
             <h2>Uncle</h2>
             <section className="flex">
-                <Cousin name={'Nazim'}></Cousin>
+                <Cousin name={'Nazim'} asset={asset}></Cousin>
                 <Cousin name={'Shova'}></Cousin>
             </section>
         </div>
