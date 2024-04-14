3868504db640f5cf10de2509e2dcd1c8458621e9
meheraz1100 <meheraz1100@gmail.com>
2024-04-14 08:41:58 +0600

    get asset and gift from myself

--- src/components/Special/Special.jsx
+++ src/components/Special/Special.jsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
+import { AssetContaxt } from "../GrandPa/Grandpa";
+
 const Special = ({asset}) => {
+    const gift = useContext(AssetContaxt)
     return (
         <div>
             <h2 className="text-2xl">Special</h2>
             <p>Has: {asset}</p>
+            <p>Also has: {gift}</p>
         </div>
     );
 };
